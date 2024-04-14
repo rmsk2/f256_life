e29d3d4ac7eb0a7a04b81cf881386021e31173c6
Martin Grap <rmsk2@gmx.de>
2024-04-14 09:28:15 +0200

    Refactor

--- tests/first.asm
+++ tests/first.asm
@@ -3,22 +3,13 @@
 
 jmp test
 
+.include "tests/mmu_setup.asm"
 .include "arith16.asm"
 .include "zeropage.asm"
 .include "world.asm"
 
 test
-    ; setup MMU, this seems to be neccessary when running as a PGZ
-    lda #%10110011                         ; set active and edit LUT to three and allow editing
-    sta 0
-    lda #%00000000                         ; enable io pages and set active page to 0
-    sta 1
-
-    ; map BASIC ROM out and RAM in
-    lda #4
-    sta 8+4
-    lda #5
-    sta 8+5
+    jsr mmuSetup
 
     jsr world.init
     brk
